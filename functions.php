6ddd42db87b667796dc49d68e45e27bba631637f
airjuice1 <airjuice1@gmail.com>
2021-11-19 14:17:14 +0300

    edit some code

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 function start_server($server_path = false, $screen_name = '', $delay = 20)
 {
 	shell_exec('echo START MINECRAFT SERVER\n');
-	shell_exec('cd ' $server_path);
+	shell_exec('cd ' . $server_path);
 	shell_exec('screen -dmS ' . $screen_name . ' java -Xmx62G -jar server.jar --nogui');
 	sleep($delay);
 	shell_exec('echo MINECRAFT SERVER STARTED\n');
