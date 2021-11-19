76bafa2453e26ae03b01287a32290d9b2f3769c6
airjuice1 <airjuice1@gmail.com>
2021-11-19 13:37:25 +0300

    edit code

--- make_bakcup.php
+++ make_bakcup.php
@@ -17,7 +17,7 @@ try
 	if ((int)$result['players']['online'] == 0)
 	{
 		// system('screen -S ' . $screen_name . ' -p 0 -X stuff "^C"');
-		$o = system('ls -lh');
+		$o = shell_exec('ls -lh');
 		echo '<pre>';
 		print_r($o);
 		echo '</pre>';
