f72b93d83022c7885dfb6cc1435a876e2f8b4bb7
airjuice1 <airjuice1@gmail.com>
2021-11-19 16:15:05 +0300

    edit some code

---
+++ ~make_backup.php
@@ -0,0 +1,34 @@
+<?php
+require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/functions.php';
+require __DIR__ . '/MinecraftPing.php';
+require __DIR__ . '/MinecraftPingException.php';
+
+use xPaw\MinecraftPing;
+use xPaw\MinecraftPingException;
+
+try
+{
+	$Query = new MinecraftPing( $server_addr, $server_port );
+	
+	$result = $Query->Query();	
+
+	if ((int)$result['players']['online'] == 0)
+	{
+		stop_server($screen_name);
+		make_archive($server_path);
+		start_server($server_path, $screen_name);
+	}
+}
+catch( MinecraftPingException $e )
+{	
+	start_server($server_path, $screen_name);
+}
+finally
+{	
+	if( $Query )
+	{
+		$Query->Close();
+	}
+}
+?>
