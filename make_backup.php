54b3f479df7db225bb962d4a7ff30ff730cdb437
airjuice1 <airjuice1@gmail.com>
2021-11-19 17:33:09 +0300

    edit some code

--- make_backup.php
+++ make_backup.php
@@ -2,6 +2,6 @@
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/functions.php';
 
-stop_server($screen_name);
+// stop_server($screen_name);
 make_archive($server_path);
-start_server($server_path, $screen_name);
+// start_server($server_path, $screen_name);
